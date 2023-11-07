b750456fb699f5ea56a9b1fda6fd4081ad7b146d
Vaida <b.vaida.b@gmail.com>
2023-11-07 08:36:09 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { sum }from './sum.js';
+console.log('labas');
+const n1 = 7;
+const n2 = 5;
+
+const n12 = 12;
+const n12 = sum(n1, n2);
+console.log(n12);
---
+++ sum.js
@@ -0,0 +1,15 @@
+function sum(a,b) {
+    return a + b;
+}
+function minu(a,b) {
+    return a - b;
+}
+function sum(a,b) {
+    return a + b;
+}
+function sum(a,b) {
+    return a + b;
+}
+
+//export default sum;
+export { sum };
